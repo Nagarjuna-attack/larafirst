13120396d9ffed1f1d758bb7004a9eeb64ac0776
Muhafandi <garamgudang1917@gmail.com>
2021-07-30 03:09:58 +0800

    asghsfasdfa

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,12 +16,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        //\App\Models\User::factory(10)->create();
-        User::create([
-            'name'     => 'Afandi',
-            'email'    => 'afandi@gmail.com',
-            'password' => bcrypt('12345')
-        ]);
+        User::factory(5)->create();
+        // User::create([
+        //     'name'     => 'Afandi',
+        //     'email'    => 'afandi@gmail.com',
+        //     'password' => bcrypt('12345')
+        // ]);
 
         Kategori::create([
             'name' => 'Jaringan',
@@ -38,64 +38,66 @@ class DatabaseSeeder extends Seeder
             'slug' => 'programming'
         ]);
 
-        Post::create([
-            'kategori_id' => 1,
-            'user_id'     => 1,
-            'title'       => 'Post Pertama',
-            'slug'        => 'post-pertama',
-            'excerpt'     => 'Ini adalah post pertama yang saya gunakan untuk menambahkan file dan nama yang akan saya gunakan dengan baik dan juga betul, karena dilihat dari kondisi parah yang menyelimuti hari ini',
-            'body'        => 'kau tidak bisa jauh dari dunia ini hanya dengan mengandalkan orang lain. orang-orang, mereka akan loyal hingga semuanya menjadi tidak berguna lagi dan kacau akan hal itu. Mengenai pada bagian yang akan kami gunakan, disitu terkadang kami menemukan semacam tempat yang cukup berbahaya dalam menjalankan aturan dan juga beberapa hal yang selalu kami gunakan untuk menambahkan beban yang ada. kecuali kami sudah menjadi kehilangan bagian pada hal yang membuat semua ini begitu kuat dan juga kacau. Dikarenakan kami selalu dapat melampaui apapun yang kami inginkan disini. itu sudah menjadi hal lumrah ketika bagian itu telah menghilang dengan sedemikian rupanya.' 
-        ]);
+        Post::factory(20)->create();
+
+        // Post::create([
+        //     'kategori_id' => 1,
+        //     'user_id'     => 1,
+        //     'title'       => 'Post Pertama',
+        //     'slug'        => 'post-pertama',
+        //     'excerpt'     => 'Ini adalah post pertama yang saya gunakan untuk menambahkan file dan nama yang akan saya gunakan dengan baik dan juga betul, karena dilihat dari kondisi parah yang menyelimuti hari ini',
+        //     'body'        => 'kau tidak bisa jauh dari dunia ini hanya dengan mengandalkan orang lain. orang-orang, mereka akan loyal hingga semuanya menjadi tidak berguna lagi dan kacau akan hal itu. Mengenai pada bagian yang akan kami gunakan, disitu terkadang kami menemukan semacam tempat yang cukup berbahaya dalam menjalankan aturan dan juga beberapa hal yang selalu kami gunakan untuk menambahkan beban yang ada. kecuali kami sudah menjadi kehilangan bagian pada hal yang membuat semua ini begitu kuat dan juga kacau. Dikarenakan kami selalu dapat melampaui apapun yang kami inginkan disini. itu sudah menjadi hal lumrah ketika bagian itu telah menghilang dengan sedemikian rupanya.' 
+        // ]);
 
 
-        Post::create([
-            'kategori_id' => 2,
-            'user_id'     => 1,
-            'title'       => 'Post Kedua',
-            'slug'        => 'post-kedua',
-            'excerpt'     => 'Ini adalah post pertama yang saya gunakan untuk menambahkan file dan nama yang akan saya gunakan dengan baik dan juga betul, karena dilihat dari kondisi parah yang menyelimuti hari ini',
-            'body'        => 'kau tidak bisa jauh dari dunia ini hanya dengan mengandalkan orang lain. orang-orang, mereka akan loyal hingga semuanya menjadi tidak berguna lagi dan kacau akan hal itu. Mengenai pada bagian yang akan kami gunakan, disitu terkadang kami menemukan semacam tempat yang cukup berbahaya dalam menjalankan aturan dan juga beberapa hal yang selalu kami gunakan untuk menambahkan beban yang ada. kecuali kami sudah menjadi kehilangan bagian pada hal yang membuat semua ini begitu kuat dan juga kacau. Dikarenakan kami selalu dapat melampaui apapun yang kami inginkan disini. itu sudah menjadi hal lumrah ketika bagian itu telah menghilang dengan sedemikian rupanya.' 
-        ]);
+        // Post::create([
+        //     'kategori_id' => 2,
+        //     'user_id'     => 1,
+        //     'title'       => 'Post Kedua',
+        //     'slug'        => 'post-kedua',
+        //     'excerpt'     => 'Ini adalah post pertama yang saya gunakan untuk menambahkan file dan nama yang akan saya gunakan dengan baik dan juga betul, karena dilihat dari kondisi parah yang menyelimuti hari ini',
+        //     'body'        => 'kau tidak bisa jauh dari dunia ini hanya dengan mengandalkan orang lain. orang-orang, mereka akan loyal hingga semuanya menjadi tidak berguna lagi dan kacau akan hal itu. Mengenai pada bagian yang akan kami gunakan, disitu terkadang kami menemukan semacam tempat yang cukup berbahaya dalam menjalankan aturan dan juga beberapa hal yang selalu kami gunakan untuk menambahkan beban yang ada. kecuali kami sudah menjadi kehilangan bagian pada hal yang membuat semua ini begitu kuat dan juga kacau. Dikarenakan kami selalu dapat melampaui apapun yang kami inginkan disini. itu sudah menjadi hal lumrah ketika bagian itu telah menghilang dengan sedemikian rupanya.' 
+        // ]);
 
 
-        Post::create([
-            'kategori_id' => 3,
-            'user_id'     => 1,
-            'title'       => 'Post Ketiga',
-            'slug'        => 'post-ketiga',
-            'excerpt'     => 'Ini adalah post pertama yang saya gunakan untuk menambahkan file dan nama yang akan saya gunakan dengan baik dan juga betul, karena dilihat dari kondisi parah yang menyelimuti hari ini',
-            'body'        => 'kau tidak bisa jauh dari dunia ini hanya dengan mengandalkan orang lain. orang-orang, mereka akan loyal hingga semuanya menjadi tidak berguna lagi dan kacau akan hal itu. Mengenai pada bagian yang akan kami gunakan, disitu terkadang kami menemukan semacam tempat yang cukup berbahaya dalam menjalankan aturan dan juga beberapa hal yang selalu kami gunakan untuk menambahkan beban yang ada. kecuali kami sudah menjadi kehilangan bagian pada hal yang membuat semua ini begitu kuat dan juga kacau. Dikarenakan kami selalu dapat melampaui apapun yang kami inginkan disini. itu sudah menjadi hal lumrah ketika bagian itu telah menghilang dengan sedemikian rupanya.' 
-        ]);
+        // Post::create([
+        //     'kategori_id' => 3,
+        //     'user_id'     => 1,
+        //     'title'       => 'Post Ketiga',
+        //     'slug'        => 'post-ketiga',
+        //     'excerpt'     => 'Ini adalah post pertama yang saya gunakan untuk menambahkan file dan nama yang akan saya gunakan dengan baik dan juga betul, karena dilihat dari kondisi parah yang menyelimuti hari ini',
+        //     'body'        => 'kau tidak bisa jauh dari dunia ini hanya dengan mengandalkan orang lain. orang-orang, mereka akan loyal hingga semuanya menjadi tidak berguna lagi dan kacau akan hal itu. Mengenai pada bagian yang akan kami gunakan, disitu terkadang kami menemukan semacam tempat yang cukup berbahaya dalam menjalankan aturan dan juga beberapa hal yang selalu kami gunakan untuk menambahkan beban yang ada. kecuali kami sudah menjadi kehilangan bagian pada hal yang membuat semua ini begitu kuat dan juga kacau. Dikarenakan kami selalu dapat melampaui apapun yang kami inginkan disini. itu sudah menjadi hal lumrah ketika bagian itu telah menghilang dengan sedemikian rupanya.' 
+        // ]);
 
 
-        Post::create([
-            'kategori_id' => 1,
-            'user_id'     => 1,
-            'title'       => 'Post Keempat',
-            'slug'        => 'post-keempat',
-            'excerpt'     => 'Ini adalah post pertama yang saya gunakan untuk menambahkan file dan nama yang akan saya gunakan dengan baik dan juga betul, karena dilihat dari kondisi parah yang menyelimuti hari ini',
-            'body'        => 'kau tidak bisa jauh dari dunia ini hanya dengan mengandalkan orang lain. orang-orang, mereka akan loyal hingga semuanya menjadi tidak berguna lagi dan kacau akan hal itu. Mengenai pada bagian yang akan kami gunakan, disitu terkadang kami menemukan semacam tempat yang cukup berbahaya dalam menjalankan aturan dan juga beberapa hal yang selalu kami gunakan untuk menambahkan beban yang ada. kecuali kami sudah menjadi kehilangan bagian pada hal yang membuat semua ini begitu kuat dan juga kacau. Dikarenakan kami selalu dapat melampaui apapun yang kami inginkan disini. itu sudah menjadi hal lumrah ketika bagian itu telah menghilang dengan sedemikian rupanya.' 
-        ]);
+        // Post::create([
+        //     'kategori_id' => 1,
+        //     'user_id'     => 1,
+        //     'title'       => 'Post Keempat',
+        //     'slug'        => 'post-keempat',
+        //     'excerpt'     => 'Ini adalah post pertama yang saya gunakan untuk menambahkan file dan nama yang akan saya gunakan dengan baik dan juga betul, karena dilihat dari kondisi parah yang menyelimuti hari ini',
+        //     'body'        => 'kau tidak bisa jauh dari dunia ini hanya dengan mengandalkan orang lain. orang-orang, mereka akan loyal hingga semuanya menjadi tidak berguna lagi dan kacau akan hal itu. Mengenai pada bagian yang akan kami gunakan, disitu terkadang kami menemukan semacam tempat yang cukup berbahaya dalam menjalankan aturan dan juga beberapa hal yang selalu kami gunakan untuk menambahkan beban yang ada. kecuali kami sudah menjadi kehilangan bagian pada hal yang membuat semua ini begitu kuat dan juga kacau. Dikarenakan kami selalu dapat melampaui apapun yang kami inginkan disini. itu sudah menjadi hal lumrah ketika bagian itu telah menghilang dengan sedemikian rupanya.' 
+        // ]);
 
 
-        Post::create([
-            'kategori_id' => 2,
-            'user_id'     => 1,
-            'title'       => 'Post Kelima',
-            'slug'        => 'post-kelima',
-            'excerpt'     => 'Ini adalah post pertama yang saya gunakan untuk menambahkan file dan nama yang akan saya gunakan dengan baik dan juga betul, karena dilihat dari kondisi parah yang menyelimuti hari ini',
-            'body'        => 'kau tidak bisa jauh dari dunia ini hanya dengan mengandalkan orang lain. orang-orang, mereka akan loyal hingga semuanya menjadi tidak berguna lagi dan kacau akan hal itu. Mengenai pada bagian yang akan kami gunakan, disitu terkadang kami menemukan semacam tempat yang cukup berbahaya dalam menjalankan aturan dan juga beberapa hal yang selalu kami gunakan untuk menambahkan beban yang ada. kecuali kami sudah menjadi kehilangan bagian pada hal yang membuat semua ini begitu kuat dan juga kacau. Dikarenakan kami selalu dapat melampaui apapun yang kami inginkan disini. itu sudah menjadi hal lumrah ketika bagian itu telah menghilang dengan sedemikian rupanya.' 
-        ]);
+        // Post::create([
+        //     'kategori_id' => 2,
+        //     'user_id'     => 1,
+        //     'title'       => 'Post Kelima',
+        //     'slug'        => 'post-kelima',
+        //     'excerpt'     => 'Ini adalah post pertama yang saya gunakan untuk menambahkan file dan nama yang akan saya gunakan dengan baik dan juga betul, karena dilihat dari kondisi parah yang menyelimuti hari ini',
+        //     'body'        => 'kau tidak bisa jauh dari dunia ini hanya dengan mengandalkan orang lain. orang-orang, mereka akan loyal hingga semuanya menjadi tidak berguna lagi dan kacau akan hal itu. Mengenai pada bagian yang akan kami gunakan, disitu terkadang kami menemukan semacam tempat yang cukup berbahaya dalam menjalankan aturan dan juga beberapa hal yang selalu kami gunakan untuk menambahkan beban yang ada. kecuali kami sudah menjadi kehilangan bagian pada hal yang membuat semua ini begitu kuat dan juga kacau. Dikarenakan kami selalu dapat melampaui apapun yang kami inginkan disini. itu sudah menjadi hal lumrah ketika bagian itu telah menghilang dengan sedemikian rupanya.' 
+        // ]);
 
 
-        Post::create([
-            'kategori_id' => 3,
-            'user_id'     => 1,
-            'title'       => 'Post Keenam',
-            'slug'        => 'post-keenam',
-            'excerpt'     => 'Ini adalah post pertama yang saya gunakan untuk menambahkan file dan nama yang akan saya gunakan dengan baik dan juga betul, karena dilihat dari kondisi parah yang menyelimuti hari ini',
-            'body'        => 'kau tidak bisa jauh dari dunia ini hanya dengan mengandalkan orang lain. orang-orang, mereka akan loyal hingga semuanya menjadi tidak berguna lagi dan kacau akan hal itu. Mengenai pada bagian yang akan kami gunakan, disitu terkadang kami menemukan semacam tempat yang cukup berbahaya dalam menjalankan aturan dan juga beberapa hal yang selalu kami gunakan untuk menambahkan beban yang ada. kecuali kami sudah menjadi kehilangan bagian pada hal yang membuat semua ini begitu kuat dan juga kacau. Dikarenakan kami selalu dapat melampaui apapun yang kami inginkan disini. itu sudah menjadi hal lumrah ketika bagian itu telah menghilang dengan sedemikian rupanya.' 
-        ]);
+        // Post::create([
+        //     'kategori_id' => 3,
+        //     'user_id'     => 1,
+        //     'title'       => 'Post Keenam',
+        //     'slug'        => 'post-keenam',
+        //     'excerpt'     => 'Ini adalah post pertama yang saya gunakan untuk menambahkan file dan nama yang akan saya gunakan dengan baik dan juga betul, karena dilihat dari kondisi parah yang menyelimuti hari ini',
+        //     'body'        => 'kau tidak bisa jauh dari dunia ini hanya dengan mengandalkan orang lain. orang-orang, mereka akan loyal hingga semuanya menjadi tidak berguna lagi dan kacau akan hal itu. Mengenai pada bagian yang akan kami gunakan, disitu terkadang kami menemukan semacam tempat yang cukup berbahaya dalam menjalankan aturan dan juga beberapa hal yang selalu kami gunakan untuk menambahkan beban yang ada. kecuali kami sudah menjadi kehilangan bagian pada hal yang membuat semua ini begitu kuat dan juga kacau. Dikarenakan kami selalu dapat melampaui apapun yang kami inginkan disini. itu sudah menjadi hal lumrah ketika bagian itu telah menghilang dengan sedemikian rupanya.' 
+        // ]);
 
     }
 }
