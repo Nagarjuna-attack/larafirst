ab2d1cea3409339ae16ec27ce3dbe14379f7c116
Muhafandi <garamgudang1917@gmail.com>
2021-08-15 02:27:14 +0800

    adding your life

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -12,7 +12,7 @@ class PostController extends Controller
         return view('beranda',[ 
         'aktif'  => 'active',
         'title' => 'Beranda',
-        'posts' => Post::latest()->get(),
+        'posts' => Post::with(['user','kategori'])->latest()->get(),
         ]);
     }
 
--- resources/views/beranda.blade.php
+++ resources/views/beranda.blade.php
@@ -1,10 +1,11 @@
 @extends('layouts.main')
 
 	@section('container')
-	<h1>Beranda</h1>
+	<h1>Beranda </h1>
 	<article>
 		@foreach($posts as $list)
 		<h2><a class="text-decoration-none" href="/read/{{ $list->slug }}">{{ $list->title }}</a></h2>
+		<h2>{{ $list->kategori->name }} by {{ $list->user->name }}</h2>
 		<h5>{{ $list->created_at }}</h5>
 		<h5>{{ $list->excerpt }}</h5>
 		@endforeach
